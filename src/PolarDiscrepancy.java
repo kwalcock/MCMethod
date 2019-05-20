1d9f68857118b3737855d442d5c4173649a630a8
Keith Alcock <github@keithalcock.com>
2019-05-19 19:21:11 -0700

    Sort areas

--- src/PolarDiscrepancy.java
+++ src/PolarDiscrepancy.java
@@ -1,13 +1,19 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class PolarDiscrepancy implements Discrepancy {
 	protected ArrayList<Double> areas = new ArrayList<Double>();
 	
 	public void addPoint(double a, double b) {
-		areas.add(area(Math.sqrt(a * a + b * b)));
+		double area = calculateArea(Math.sqrt(a * a + b * b));
+		int index = Collections.binarySearch(areas, area);
+		
+		if (index < 0)
+			index = -index - 1;
+		areas.add(index, area);
 	}
 	
-	public double area(double r) {
+	public double calculateArea(double r) {
 		double quarter = Math.PI * r * r / 4;
 		if (r <= 1)
 			return quarter;
@@ -19,11 +25,8 @@ public class PolarDiscrepancy implements Discrepancy {
 	public double calculate(int p) {
 		double d = 0;
 		for (int i = 0; i < p; i++) {
+			double actualInside = i;
 			double area = areas.get(i);
-			double actualInside = 0;
-			for (int j = 0; j < p; j++)
-				if (areas.get(j) < area)
-					actualInside++;
 			double expectedInside = area * (p - 1);
 			double difference = actualInside - expectedInside;
 			d += difference * difference;
@@ -53,6 +56,6 @@ public class PolarDiscrepancy implements Discrepancy {
 		PolarDiscrepancy polarDiscrepancy = new PolarDiscrepancy();
 		
 		for (double r: radii)
-			System.out.println("" + r + "\t" + polarDiscrepancy.area(r));
+			System.out.println("" + r + "\t" + polarDiscrepancy.calculateArea(r));
 	}
 }
